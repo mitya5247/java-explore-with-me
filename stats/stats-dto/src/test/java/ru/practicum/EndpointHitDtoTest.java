cfe42a3e874aa3dc0d31517edff2c5a71934b97b
mitya5247 <mitya5247@yandex.ru>
2024-08-03 23:38:56 +0300

    style: правки по codestyle

--- stats/stats-dto/src/test/java/ru/practicum/EndpointHitDtoTest.java
+++ stats/stats-dto/src/test/java/ru/practicum/EndpointHitDtoTest.java
@@ -1,38 +1,4 @@
 package ru.practicum;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
-public class EndpointHitDtoTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public EndpointHitDtoTest(String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( EndpointHitDtoTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class EndpointHitDtoTest {
 }
