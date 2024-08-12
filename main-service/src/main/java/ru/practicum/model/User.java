cddd10754645311b11e3af66d56ede56155f4952
mitya5247 <mitya5247@yandex.ru>
2024-08-12 15:26:56 +0300

    feat: добавил функционал с пользовтелями

--- main-service/src/main/java/ru/practicum/model/User.java
+++ main-service/src/main/java/ru/practicum/model/User.java
@@ -3,15 +3,26 @@ package ru.practicum.model;
 import lombok.Data;
 import org.springframework.validation.annotation.Validated;
 
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
+@Entity
+@Table(name = "users")
 @Validated
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
     @NotBlank(message = "name must not be null or empty")
+    @Column(name = "name")
+    @Size(min = 2, max = 250)
     String name;
+    @NotBlank
     @Email
+    @Column(name = "email")
+    @Size(min = 6, max = 254)
     String email;
 }
