0fa18c8b8ea361e50f26d309d353bbb92d232b53
mitya5247 <mitya5247@yandex.ru>
2024-08-18 03:48:16 +0300

    feat: исправил баг со StackOverFlow при вызове из БД Сompilation

--- main-service/src/main/java/ru/practicum/mapper/CompilationMapper.java
+++ main-service/src/main/java/ru/practicum/mapper/CompilationMapper.java
@@ -4,22 +4,27 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
 import ru.practicum.model.compilation.Compilation;
 import ru.practicum.model.compilation.dto.CompilationResponseDto;
 import ru.practicum.model.compilation.dto.NewCompilationDto;
 import ru.practicum.model.event.Event;
+import ru.practicum.model.event.dto.EventDtoResponse;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface CompilationMapper {
 
-    @Named("eventToId")
-    default Integer eventToId(Event event) {
-        return event.getId();
+    EventMapper eventMapper = Mappers.getMapper(EventMapper.class);
+
+    @Named("eventToResponse")
+    default EventDtoResponse eventToResponse(Event event) {
+        return eventMapper.eventToEventDtoResponse(event);
     }
 
+    @Mapping(target = "events", ignore = true)
     CompilationResponseDto newToComplitaionResponse(NewCompilationDto newCompilationDto);
 
-    @Mapping(target = "events", source = "compilation.events", qualifiedByName = "eventToId")
+    @Mapping(target = "events", source = "compilation.events", qualifiedByName = "eventToResponse")
     CompilationResponseDto compilationToCompilationResponse(Compilation compilation);
 
     @Mapping(target = "events", ignore = true)
